caffdd3bb9697d7686c1b1c2a48bbe69aa50fd7d
VeraBelugina <vryshanchik-cherry@mail.ru>
2024-11-03 20:52:27 +0300

    Add files via upload

---
+++ mod9_t6.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    n = len(text)
+    for x in range(n):
+        for y in range(x+1, n+1):
+            yield text[x:y]
+
+
+a = all_variants("abс")
+for i in a:
+    print(i)
